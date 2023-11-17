0775ff61cd79bb2dfe5eacd0d5b8bdd76901df77
Alumno <Alumno@LAB1509-10>
2023-11-17 17:35:42 -0500

    Agregar archivos de proyecto.

---
+++ lab09/lab09/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace lab09
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            TapBtn.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TapDemo());
+            };
+            PinchBtn.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PinchDemo());
+            };
+            PanBtn.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PanDemo());
+            };
+            SwipeBtn.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new SwipeDemo());
+            };
+        }
+    }
+}
